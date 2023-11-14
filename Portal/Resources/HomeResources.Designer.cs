de985d51098654aeb7283a6791b0628c8a4291cf
Nicholas Peters <nikp75@gmail.com>
2023-11-14 15:53:10 +0000

    Renamed the Web project to Portal

--- Portal/Resources/HomeResources.Designer.cs
+++ Portal/Resources/HomeResources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Web.Resources {
+namespace Portal.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Web.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Web.Resources.HomeResources", typeof(HomeResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Portal.Resources.HomeResources", typeof(HomeResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
