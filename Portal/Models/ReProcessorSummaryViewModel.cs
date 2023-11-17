a5fbec9bbb48686b0a518858f955851e931f3873
Saqlain Malik <saqlain.malik.external@atos.net>
2023-11-17 15:38:46 +0000

    259477: Created full UI page, with hardcoded text

---
+++ Portal/Models/ReProcessorSummaryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Portal.Models
+{
+    public class ReProcessorSummaryViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string ContactName { get; set; } = string.Empty;
+        public string AccountNumber { get; set; } = string.Empty;
+    }
+}
