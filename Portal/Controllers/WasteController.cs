5c556eb10d941c19f7c9badc9cf0212ebb6e000d
AtosSK <sajid.khurshid@atos.net>
2023-11-20 11:00:03 +0000

    Inital Commit.

---
+++ Portal/Controllers/WasteController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PRN.Web.Models;
+using System.Diagnostics;
+
+namespace PRN.Web.Controllers
+{
+    public class WasteController : Controller
+    {
+        private readonly ILogger<WasteController> _logger;
+
+        public WasteController(ILogger<WasteController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // return view 
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
