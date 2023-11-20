b76a72c87041e0ae2e8ad3e08898812abbe8c55e
Prasad <prasad.cherukuri.external@atos.net>
2023-11-20 16:02:45 +0000

    Segregate responsibilities

--- Portal/Helpers/CultureHelper.cs
+++ Portal/Helpers/CultureHelper.cs
@@ -1,12 +1,18 @@
 ﻿using PRN.Web.Constants;
+using Microsoft.AspNetCore.Localization;
 
 namespace Portal.Helpers
 {
     public static class CultureHelper
     {
-        public static (string oppositeCultureValue, string oppositeCultureName) GetCultureInfo(HttpContext context)
+        public static (string oppositeCultureValue, string oppositeCultureName) GetCultureInfo(IHttpContextAccessor httpContextAccessor)
         {
-            var requestCultureInfo = context.Features.Get<Microsoft.AspNetCore.Localization.IRequestCultureFeature>()?.RequestCulture.Culture;
+            if (httpContextAccessor == null || httpContextAccessor.HttpContext == null)
+            {
+                throw new InvalidOperationException("HttpContext is null. The operation requires a valid HttpContext.");
+            }
+
+            var requestCultureInfo = httpContextAccessor.HttpContext.Features.Get<IRequestCultureFeature>()?.RequestCulture?.Culture;
             var isEnglish = Constants.CultureConstants.English.Name == requestCultureInfo?.Name;
             var oppositeCultureValue = isEnglish ? Constants.CultureConstants.Welsh.Name : Constants.CultureConstants.English.Name;
             var oppositeCultureName = isEnglish ? "Welsh" : "English";
