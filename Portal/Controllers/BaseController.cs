0ce1eff8e69f92bc7feb72caa27262d6a3d9cd24
AtosSaqlain <150350072+AtosSaqlain@users.noreply.github.com>
2023-11-22 16:57:59 +0000

    259477 homepage (#11)
    * 259477: Created full UI page, with hardcoded text
    
    * 259477: Created partial view to display cards
    
    * 259477: Changed partial view to async
    
    * 259477: code cleanup
    
    * 259477: merged from dev branch, and also created welsh resource file

---
+++ Portal/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PRN.Web.Models;
+using System.Diagnostics;
+
+namespace Portal.Controllers
+{
+    public class BaseController<T> : Controller
+    {
+        protected readonly ILogger<T> logger;
+
+        public BaseController(ILogger<T> logger) 
+        {
+            this.logger = logger;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
