f0b62adb622fa1334324c7664d97fd5abba11256
Shehzad Ismail <shehzad.ismail@atos.net>
2023-11-28 11:49:20 +0000

    [PRN-259552: Added initial tests for the WasteController in the API]

---
+++ EPRN.UnitTests/API/Controllers/WasteControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Waste.API.Controllers;
+using Waste.API.Services.Interfaces;
+
+namespace EPRN.UnitTests.API.Controllers
+{
+    [TestClass]
+    public class WasteControllerTests
+    {
+        private WasteController _wasteController;
+        private Mock<IWasteService> _mockWasteService;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _mockWasteService = new Mock<IWasteService>();
+            _wasteController = new WasteController(_mockWasteService.Object);
+        }
+
+        [TestMethod]
+        public async Task TestWasteType_Returns_OK()
+        {
+            //Arrange
+            var journeyId = 3;
+
+            //Act
+            var result = await _wasteController.WasteType(journeyId);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+        }
+
+        [TestMethod]
+        public async Task TestWasteType_ReturnsBadRequest_WhenNoIdSupplied()
+        {
+            //Arrange
+
+            //Act
+            var result = await _wasteController.WasteType(null);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+        }
+    }
+}
