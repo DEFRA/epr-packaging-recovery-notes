6b57de59f29d9c9e796f81c1f14b9180f8136b4d
AtosSK <sajid.khurshid@atos.net>
2023-11-30 14:51:02 +0000

    Added language resources.

--- Portal/Resources/BaledWithWireResource.cy-GB.Designer.cs
+++ Portal/Resources/BaledWithWireResource.cy-GB.Designer.cs
@@ -22,14 +22,14 @@ namespace Portal.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class BaledWithWireResource {
+    public class BaledWithWireResource___Copy {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal BaledWithWireResource() {
+        internal BaledWithWireResource___Copy() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Portal.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Portal.Resources.BaledWithWireResource", typeof(BaledWithWireResource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Portal.Resources.BaledWithWireResource - Copy", typeof(BaledWithWireResource___Copy).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
