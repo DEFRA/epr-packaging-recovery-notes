1b29be83adcd6894dd0b4102f19c54bdd7f1bdc7
AtosSK <sajid.khurshid@atos.net>
2023-12-05 16:40:03 +0000

    Changes for portal ui.

---
+++ Waste.API/Migrations/20231205160346_ChangedDoneWasteToString.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Waste.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedDoneWasteToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DoneWaste",
+                table: "WasteJourney",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DoneWaste",
+                table: "WasteJourney",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
