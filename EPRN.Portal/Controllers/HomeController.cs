65bf1e0703737af776ddb7be1a0480d693107ea8
Shehzad Ismail <shehzad.ismail@atos.net>
2023-12-13 12:56:28 +0000

    [PRN-277268: WIP styling]

--- EPRN.Portal/Controllers/HomeController.cs
+++ EPRN.Portal/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace EPRN.Portal.Controllers
 
         public IActionResult Index()
         {
-            var reProcessorViewModel = new ReProcessorSummaryViewModel { Name = "Green LTD", ContactName = "John Watson", AccountNumber = "A/c 1234" };
+            var reProcessorViewModel = new ReProcessorSummaryViewModel { Name = "Green LTD", ContactName = "John Watson", AccountNumber = "12 Head office St, Liverpool, L12 345 - 0098678" };
 
             return View(reProcessorViewModel);
         }
