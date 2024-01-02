b900f07485fb22d314254dc49e2d9c8a2d2a3d6b
Shehzad Ismail <shehzad.ismail@atos.net>
2024-01-02 14:55:41 +0000

    [PRN-277268-unit-tests: Added unit test for HomeController]

---
+++ EPRN.UnitTests/Portal/Controllers/HomeControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using EPRN.Portal.Controllers;
+using EPRN.Portal.Helpers.Interfaces;
+using EPRN.Portal.Services.Interfaces;
+using Moq;
+
+namespace EPRN.UnitTests.Portal.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        private HomeController _homeController;
+        private Mock<IHomeService> _mockHomeService;
+        private Mock<IHomeServiceFactory> _mockHomeServiceFactory;
+
+
+        [TestInitialize]
+        public void Init()
+        {
+            _mockHomeService = new Mock<IHomeService>();
+            _mockHomeServiceFactory = new Mock<IHomeServiceFactory>();
+            _mockHomeServiceFactory.Setup(s => s.CreateHomeService()).Returns(_mockHomeService.Object);
+            _homeController = new HomeController(_mockHomeServiceFactory.Object);
+        }
+
+
+        [TestMethod]
+        public async Task Index_Calls_Correct_Service_Method_Once()
+        {
+            // Arrange
+
+            // Act
+            var result = await _homeController.Index();
+
+            // Assert
+            _mockHomeService.Verify(s => s.GetHomePage(), Times.Once);
+        }
+
+    }
+}
