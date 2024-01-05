59e108532976ad30ca2f899fbcd5e539d2730135
atos-ajoyce <alex.joyce.external@atos.net>
2024-01-05 14:51:01 +0000

    rem and readd project

---
+++ EPRN.Common.Data/DataModels/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace EPRN.Common.Data.DataModels
+{
+    public abstract class BaseEntity : IdBaseEntity
+    {
+        public DateTime CreatedDate { get; set; }
+
+        public string CreatedBy { get; set; } = string.Empty;
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public string LastModifiedBy { get; set; } = null;
+    }
+}
---
+++ EPRN.Common.Data/DataModels/WasteSubType.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EPRN.Common.Data.DataModels
+{
+    public class WasteSubType : IdBaseEntity
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = null;
+
+        public double? Adjustment { get; set; } = null;
+
+        public int WasteTypeId { get; set; }
+
+        public bool AdjustmentPercentageRequired { get; set; }
+
+        public WasteType WasteType { get; set; }
+
+        public ICollection<WasteJourney> WasteJourneys { get; set; }
+    }
+}
