46937ffcce23a2bd6a3b2282443cfde5c9ef8af8
Shehzad Ismail <shehzad.ismail@atos.net>
2024-01-08 12:22:52 +0000

    [PRN-276925: Filter drop down now working + minor custom CSS added]

---
+++ EPRN.Portal/ClientApp/js/pages/Prn-Create.js
@@ -0,0 +1,22 @@
+﻿export default (() => {
+
+    $(document).ready(function () {
+        $('#sort').on('change', function () {
+
+            var dropDownValue = $(this).val();
+
+            $('.prnTables').removeClass('govuk-visually-hidden');
+
+            switch (dropDownValue) {
+                case 'reprocessor':
+                    $('.exporter').addClass('govuk-visually-hidden');
+                    break;
+                case 'exporter':
+                    $('.reprocessor').addClass('govuk-visually-hidden');
+                    break;
+            };
+
+        });
+    })
+       
+})();
