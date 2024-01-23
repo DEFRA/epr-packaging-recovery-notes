0d6dbaa08ba5003846091915f23c54605badde3b
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-01-23 11:35:49 +0000

    [PRN-282472: Added TODO comment]

--- EPRN.Portal/Services/PRNService.cs
+++ EPRN.Portal/Services/PRNService.cs
@@ -128,6 +128,7 @@ namespace EPRN.Portal.Services
 
         public async Task<PrnSavedAsDraftViewModel> GetDraftPrnConfirmationModel(int id)
         {
+            //TODO: The PrnNumber property needs to be system generated in the future
             return new PrnSavedAsDraftViewModel
             {
                 Id = id,
