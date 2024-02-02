8477facda2997a65d9f12bcf6aa7ac08ebc7c7b4
atos-ajoyce <alex.joyce.external@atos.net>
2024-02-02 14:11:13 +0000

    sync enums

--- EPRN.Common.Data/Enums/PrnStatus.cs
+++ EPRN.Common.Data/Enums/PrnStatus.cs
@@ -2,13 +2,16 @@
 {
     public enum PrnStatus
     {
-        Draft,
-        CheckYourAnswersComplete,
-        Sent, // sent to a producer/compliance scheme
-        Accepted, // accepted by the producer/compliance scheme
-        CancellationRequested,
+        // DO NOT change the order of the current enum items, new items can be added to the end
+        Draft = 0,
+        CheckYourAnswersComplete = 1,
+        // sent to a producer/compliance scheme
+        Sent = 2,
+        // accepted by the producer/compliance scheme - this is the last step of the process therefore no need for Completed
+        Accepted = 3,
+        CancellationRequested = 4,
+        Cancelled = 5,
         Complete,
-        Cancelled,
         AwaitingAcceptance,
         Rejected,
         AwaitingCancellation
