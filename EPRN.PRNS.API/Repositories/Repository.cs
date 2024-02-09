ead64ff6ad57d27c52872d905a693e837825f0f4
atos-ajoyce <alex.joyce.external@atos.net>
2024-02-09 13:41:30 +0000

    rem commented code

--- EPRN.PRNS.API/Repositories/Repository.cs
+++ EPRN.PRNS.API/Repositories/Repository.cs
@@ -159,8 +159,7 @@ namespace EPRN.PRNS.API.Repositories
             var recordsPerPage = request.PageSize;
             var prns = _prnContext.PRN
                 .Include(repo => repo.WasteType)
-                .Include(repo => repo.PrnHistory)//.Where(history => history.Status >= PrnStatus.Accepted))
-                //.AsQueryable();
+                .Include(repo => repo.PrnHistory)
                 .Where(prn => prn.PrnHistory.Any(h => h.Status >= PrnStatus.Accepted));
 
             if (!string.IsNullOrWhiteSpace(request.FilterBy))
